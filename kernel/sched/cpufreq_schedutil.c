9b0cb489fa1e713962d7abb990adc004f01464dd
kakarot-super <wx3446480369@qq.com>
2026-05-04 12:50:19 +0800

    Revert "sched/util: add busy detection to prevent premature frequency drops"
    This reverts commit 5f54956c581eb5f5cf4e469c4e1e0d40eb7661b2.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1014,12 +1014,10 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 	 * Do not reduce the frequency if the CPU has not been idle
 	 * recently, as the reduction is likely to be premature then.
 	 */
-	if (!uclamp_rq_is_capped(cpu_rq(sg_cpu->cpu)) &&
-	    sugov_cpu_is_busy(sg_cpu) && 
-	    next_f < sg_policy->next_freq && 
-	    !sg_policy->need_freq_update) {
-		
+	if (busy && next_f < sg_policy->next_freq) {
 		next_f = sg_policy->next_freq;
+
+		/* Restore cached freq as next_freq has changed */
 		sg_policy->cached_raw_freq = sg_policy->prev_cached_raw_freq;
 	}
 
